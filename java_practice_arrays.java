305fc16e688047bec03b2335dbae5d16380f3563
rohith1999 <30693017+rohith1999@users.noreply.github.com>
2021-09-20 17:59:28 +0530

    Add files via upload

---
+++ java_practice_arrays.java
@@ -0,0 +1,39 @@
+package com.thbs;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // arrays
+//        int arr[] = new int[10];
+//        //insert values
+//        for (int i = 0; i < arr.length; i++) {
+//            arr[i] = i;
+//        }
+//        //insert values 1
+//        int arr1[]={10,20,30,40,50};
+//        //display values
+//       for (int i = 0; i < arr.length; i++) {
+//            System.out.println(arr[i]);
+//        }
+
+       //take user input
+//        Scanner sc=new Scanner(System.in);
+//        System.out.println("enter no of elelments");
+//        int n=sc.nextInt();
+//        int userInputArray []= new int[n];
+//        int sum = 0;
+//        System.out.println("enter values for the array");
+//        for (int i=0;i<n;i++){
+//            userInputArray[i]=sc.nextInt();
+//            sum+=userInputArray[i];
+//        }
+//        //display
+//        System.out.println(sum);
+//        for (int i = 0; i < userInputArray.length; i++) {
+//            System.out.println(userInputArray[i]);
+//        }
+
+    }
+}
