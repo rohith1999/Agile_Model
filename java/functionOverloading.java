b229a4a4e417b929024128cba96cbc3bf3dde41d
rohith1999 <30693017+rohith1999@users.noreply.github.com>
2021-09-22 15:26:06 +0530

    Update functionOverloading.java

--- java/functionOverloading.java
+++ java/functionOverloading.java
@@ -14,28 +14,100 @@ public class Summation {
     }
 
 }
+//average
+package com.thbs.util;
+
+public class Average {
+    public double calculateAverage(int... numbers) {
+        int sum = 0;
+        for (int n : numbers) {
+            sum += n;
+        }
+        return (sum / numbers.length);
+
+    }
+
+
+    public double calculateAverage(double d,int... numbers) {
+        double sum = d;
+        for (int n : numbers) {
+            sum += n;
+        }
+        return (sum / (numbers.length+1));
+
+    }
+
+    public double calculateAverage(double d, long l,int... numbers) {
+        double sum = d+l;
+        for (int n : numbers) {
+            sum += n;
+        }
+        return (sum / (numbers.length+2));
+
+    }
+
+}
+
+package com.thbs.util;
+
+public class Tax {
+
+    public double calculateTax(int amount, String status){
+        return (amount*0.1);
+    }
+    public double calculateTax(int amount){
+        return (amount*0.25);
+    }
+
+}
+
+
 
 //main
 package com.thbs.mainpack;
 
+import com.thbs.util.Average;
 import com.thbs.util.Summation;
+import com.thbs.util.Tax;
 
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        Summation summation=new Summation();
-        System.out.println( summation.add(1,2));
-        System.out.println(summation.add("hey,","I'm rohith"));
-        System.out.println(summation.add(1,16.5F,5.9));
+//        Summation summation=new Summation();
+//        System.out.println( summation.add(1,2));
+//        System.out.println(summation.add("hey,","I'm rohith"));
+//        System.out.println(summation.add(1,16.5F,5.9));
+
+//        Tax tax  =new Tax();
+//        System.out.println(tax.calculateTax(20000,"consultant"));
+//        System.out.println(tax.calculateTax(10000));
+
+        Average average = new Average();
+        int[] arr={1,2,3,4,5,6,7,8,9,10};
+        System.out.println(average.calculateAverage(1,2,3,4,5,6,7,8,9,10));
+        System.out.println(average.calculateAverage(4.75,10L,arr));
+        System.out.println(average.calculateAverage(4.75,arr));
+        System.out.println(average.calculateAverage(arr));
+
+
 
 
     }
 }
 
-//output
-3
-hey, I'm rohith
-23.4
+// output
+// 3
+// hey, I'm rohith
+// 23.4
 
-Process finished with exit code 0
+// Process finished with exit code 0
+
+// //tax
+// 2000.0
+// 2500.0
+
+// 5.0	
+// 5.8125
+// 5.431818181818182
+// 5.0
