84248fcc73e08a401eb0a6daea8cc5f44fb7047d
rohith1999 <30693017+rohith1999@users.noreply.github.com>
2021-09-27 15:19:01 +0530

    Update stack.java

--- java/stack.java
+++ java/stack.java
@@ -18,11 +18,19 @@ public class Main {
         System.out.println(stack);
 
         //top element
-        System.out.println(stack.peek());   
-        
+        System.out.println(stack.peek());
+
+        System.out.println(stack.search("ccccc"));
+
+        System.out.println(stack.search("b"));
+
+        System.out.println(stack.contains("b"));
+        System.out.println(stack.contains("aaa"));
+
         //stack is a subclass of vector
-       // Vector<String> stacks=new Stack<>();
-        
+        // Vector<String> stacks=new Stack<>();
+        //stacks.add("aaa");
+
 
 
     }
