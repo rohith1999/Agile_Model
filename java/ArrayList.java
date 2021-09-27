7c807788737a98a6bedcbf2f010035997426fc5e
rohith1999 <30693017+rohith1999@users.noreply.github.com>
2021-09-27 14:05:28 +0530

    Update ArrayList.java

--- java/ArrayList.java
+++ java/ArrayList.java
@@ -44,3 +44,115 @@ public class Main {
 
     }
 }
+
+
+//arraylist basics
+package com.company;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+
+        //List
+        //root interface is collection
+        //list is the subclass of collection
+        //Implemented classes
+        //ArrayList
+        //linked list
+        //Vector
+
+        //features allows duplicate elements
+        //insert and delete using position
+
+//        ArrayList<String> arrayList = new ArrayList<>();
+//        arrayList.add("amith");
+//        arrayList.add("amith");
+//        arrayList.add("rohith");
+//        arrayList.add("manju");
+//        arrayList.add("ramesh");
+//        //insert at index 0 --
+//        arrayList.add(0, "mahesh");
+//        //remove at index position
+//        arrayList.remove(1);
+//        //remove using data
+//        arrayList.remove("mahesh");
+//        System.out.println(arrayList);
+//
+//        //merge two arrays
+//        ArrayList<String> subList = new ArrayList<>();
+//        subList.add("one");
+//        subList.add("two");
+//        subList.add("three");
+//
+//        //with index position
+//        arrayList.addAll(arrayList.size() - 1, subList);
+//
+//        System.out.println(arrayList);
+//
+//
+//        //removes all elements
+//        //  arrayList.clear();
+//
+//
+//        //traditional for loop
+////        for (int i=0;i<arrayList.size();i++){
+////            System.out.println(arrayList.get(i));
+////        }
+////
+////        //enhanced for  loop
+////        for (String e:arrayList){
+////            System.out.println(e);
+////        }
+//
+//        //iterator
+//        Iterator<String> it = arrayList.iterator();
+//        while (it.hasNext()) {
+//            System.out.print(it.next().toUpperCase()+" ");
+//        }
+
+
+        //inteer array
+
+        ArrayList<Integer> integers=new ArrayList<>();
+        Scanner scanner=new Scanner(System.in);
+
+        System.out.println("enter size of elements");
+        int size =  scanner.nextInt();
+
+        System.out.println("Enter elements");
+
+        for (int i=0;i<size;i++){
+            integers.add(scanner.nextInt());
+        }
+
+//        int sum=0;
+//        for (Integer i:integers){
+//            sum+=i;
+//            System.out.println(i);
+//        }
+//        System.out.println("Sum is: "+sum);
+
+        scanner.close();
+
+        //using iterator
+        Iterator<Integer> it= integers.iterator();
+        int sum = 0;
+        while (it.hasNext()){
+            int ele = it.next();
+            sum+= ele;
+            System.out.println(ele);
+        }
+        System.out.println("++++++++++"+sum);
+
+     
+
+
+    }
+}
+
