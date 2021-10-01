c7bf145b1f6000742ab8c8f2bd4c32c4fb330326
rohith1999 <30693017+rohith1999@users.noreply.github.com>
2021-10-01 15:07:00 +0530

    Update contents.java

--- java/caseStudy/contents.java
+++ java/caseStudy/contents.java
@@ -74,3 +74,18 @@ public class Train
                 '}';
     }
 }
+
+
+//dao
+import java.sql.*;
+ 
+public class Train_DAO {
+    public static Connection con = DBManager.getConnection();
+ 
+    public static Train findTrain(int trainNum) throws SQLException {
+        Statement st = con.createStatement();
+        ResultSet rs = st.executeQuery("select * from trains where train_no =" + trainNum);
+        rs.next();
+        return new Train(rs.getInt(1), rs.getString(2), rs.getString(3), rs.getString(4), rs.getDouble(5));
+    }
+}
