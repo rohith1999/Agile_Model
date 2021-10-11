bb091f78660e49572c26e3e92e08b2f59b36fe3a
rohith1999 <30693017+rohith1999@users.noreply.github.com>
2021-10-11 15:57:43 +0530

    Update controller.java

--- controller.java
+++ controller.java
@@ -1,20 +1,4 @@
-[15:12] Mythili G (Guest)
-<form action="/SampleSpringMVC/GreetPage" method = "post">
 
- <B>Enter UserName </B><input type = "text" name = "name" /><br>
-
- <B>Enter Password </B><input type = "password" name = "pass" /><br> <input type = "submit" value = "submit"/>
-
-</form>
-
-[15:15] Mythili G (Guest)
-<% String name = (String) request.getAttribute("name"); %> <%out.println(name); %><br>
-
-${​​​​name}​​​​
-
-${​​​​pass}​​​​
-
-[15:17] Mythili G (Guest)
 package controller; import org.springframework.stereotype.Controller;
 
 import org.springframework.ui.ModelMap;
@@ -27,23 +11,23 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import org.springframework.web.servlet.ModelAndView; @Controller
 
-public class Customercontroller {​​​​
+public class Customercontroller {
 
     @RequestMapping(value="/LoginForm", method=RequestMethod.GET)
 
     public  ModelAndView getLoginForm()
 
-    {​​​​
+    {
 
         ModelAndView model = new ModelAndView("loginform");
 
         return model;
 
-    }​​​​     /*@RequestMapping(value="/GreetPage", method=RequestMethod.POST)
+    }    /*@RequestMapping(value="/GreetPage", method=RequestMethod.POST)
 
     public ModelAndView submitLoginForm(@RequestParam("name") String name,@RequestParam("pass") String pass)
 
-    {​​​​
+    {
 
         ModelAndView model = new ModelAndView("greetpage");
 
@@ -55,13 +39,13 @@ public class Customercontroller {​​​​
 
         return model;
 
-    }​​​​*/
+    }*/
 
     @RequestMapping(value="/GreetPage", method=RequestMethod.POST)
 
     public String submitLoginForm1(@RequestParam("name") String name,@RequestParam("pass") String pass,ModelMap map)
 
-    {​​​​
+    {
 
         map.addAttribute("name",name);
 
@@ -69,5 +53,5 @@ public class Customercontroller {​​​​
 
         return "greetpage";
 
-    }​​​​ }​​​​
+    }}
 
